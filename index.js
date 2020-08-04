e8ebc45fa07d906dac84e62b79772b47bc5c08cd
Nikita <nkdevinfo@gmail.com>
2020-08-04 14:40:56 +0300

    returns a modified clone of chunk (thanks https://github.com/ifedu/gulp-modify-file/pull/3)

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const through = require( 'through2' );
 module.exports = function ( fn ) {
     return through.obj( function ( chunk, enc, callback ) {
         const contents = fn( String( chunk.contents ), chunk.path, chunk ) || chunk.contents;
+        const modifiedChunk = chunk.clone();
 
         if ( true === chunk.isBuffer() ) {
-            chunk.contents = new Buffer.from( contents );
+            modifiedChunk.contents = new Buffer.from( contents );
         }
 
-        callback( null, chunk );
+        callback( null, modifiedChunk );
     } );
 };
